b77ec906a56c825b2155ce7eefc571f1c75fce38
Dave Oster <daveosterjr@gmal.com>
2014-05-21 11:34:16 -0500

    google analytics

--- snippets/header.php
+++ snippets/header.php
@@ -5,7 +5,8 @@
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">    
-    <meta name="description" content="">
+    <meta name="description" content="With 30 ft. ceilings and 10,000 sq. ft. of glass, steel, and concrete, let this award winning building be the venue for your next event." />
+
     <link rel="icon" href="../favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
@@ -15,6 +16,20 @@
     <link href="../assets/css/bootstrap-responsive.min.css" rel="stylesheet">
     <link href="../assets/css/font-awesome.min.css"  rel="stylesheet">
     <link href="../assets/css/style.css" rel="stylesheet">
+			
+		<script type="text/javascript">
+		
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-20164022-4']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+		</script>
 
   </head>
 
