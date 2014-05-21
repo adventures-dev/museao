94dcdbc0d0453f8d05b7ca2864326c2b97ff1d0f
Dave Oster <daveosterjr@gmal.com>
2014-05-20 19:28:06 -0500

    home page complete

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Museao | Event Hosting | Columbia, MO</title>
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="description" content="">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Josefin+Sans:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic|Mr+De+Haviland' rel='stylesheet' type='text/css'>
+        <!-- Le styles -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+		
+    </head>
+    
+    <body>
+		<div id="landing_page">
+			
+			<div id="navbar">
+				<div id="logo">
+					<a href="../"><img src="../assets/img/logo.png"></a>
+				</div>
+				<div id="nav_buttons">
+					<a href="#">
+						About Venue
+					</a>
+					<a href="#">
+						Preferred Vendors
+					</a>
+					<a href="#">
+						Contact
+					</a>
+				</div>
+			</div>
+			
+			<div id="mid_section">
+				<h1>Where <span>Beauty</span> Meets Functionality</h1>
+				<h2>Modern Event Venue</h2>
+				<a id="call_to_action" href="#">Contact us</a>
+			</div>
+			
+			<div id="landing_footer">
+				<div id="text_section">
+					<a href="mailto:museaoevents@gmail.com"><li>museaoevents@gmail.com</li></a>
+					<li>573.445.0678</li>
+					
+				</div>
+				<div id="social_section">
+					<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+					<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+				</div>
+			</div>
+
+		</div>
+        
+        
+	    <!-- Le scripts -->
+        <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+        <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/placeholder-fix.js"></script>
+
+        <!-- custom scripts -->            
+        <script type="text/javascript">
+        	
+        
+        </script><!-- end of custom scripts -->
+                
+    </body>
+</html>
